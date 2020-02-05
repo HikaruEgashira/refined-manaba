9b7c3111799c2c3cc35a57c1da5876604af0d7cb
HikaruEgashira <shighten@gmail.com>
2020-02-05 21:11:34 +0900

    classで実装

--- src/0_domain/auth.ts
+++ src/0_domain/auth.ts
@@ -1,5 +1,4 @@
-import { Option } from 'fp-ts/lib/Option';
-import { Url } from './parser';
+import { Option, none, some } from 'fp-ts/lib/Option';
 
 export interface Auth {
   state: Option<boolean>;
@@ -7,6 +6,44 @@ export interface Auth {
   passWord: Option<string>;
 }
 
-export type IsLogin = (url: Url) => Promise<Auth>;
-export type GetCokkie = (loginUrl: Url) => Promise<Auth>;
-export type RemoveCookie = (path: string) => void;
+class User implements Auth {
+  state: Option<boolean>;
+  userName: Option<string>;
+  passWord: Option<string>;
+
+  constructor({ passWord, userName }: { passWord: string; userName: string }) {
+    this.state = none;
+    this.passWord = some(passWord);
+    this.userName = some(userName);
+  }
+
+  public value: Auth = {
+    state: this.state,
+    userName: this.userName,
+    passWord: this.passWord
+  };
+
+  public updateUser(auth: Auth) {
+    this.state = auth.state;
+    this.passWord = auth.passWord;
+    this.userName = auth.userName;
+    return this.value;
+  }
+
+  public login() {
+    this.state = some(true);
+    return this.value;
+  }
+
+  public logout() {
+    this.state = some(false);
+    return this.value;
+  }
+
+  public deleteUser() {
+    this.state = none;
+    this.userName = none;
+    this.passWord = none;
+    return this.value;
+  }
+}
